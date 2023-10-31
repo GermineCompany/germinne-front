a2de99cb7175b0181ad2cb3c2401c1d14aa38dce
Vitor Moutim <moutimg@gmail.com>
2023-10-31 14:18:33 -0300

    fix: typo
    Co-authored-by: João Pedro Loureiro <49416499+Dev-Loureiro@users.noreply.github.com>

--- src/pages/SobreNos/SobreNos.jsx
+++ src/pages/SobreNos/SobreNos.jsx
@@ -10,7 +10,7 @@ function SobreNos() {
     <article>
       <div>
         <h3>Sobre nós</h3>
-        <p>A Germine surgiu com o objetivo de incentivar cada vêz mais pessoas parte de um movimento que transforma os espaços urbanos em fontes de vida e saúde. Nossa plataforma promove informações e incentiva iniciativas que promovam a agricultura urbana: Leia os mais diversos artigos em nosso blog, converse com nosso chatbot inteligente que irá te orientar e ajudar a resolver os seus problemas, invista em nosso kit de cultivo indoor e plante em qualquer lugar de maneira simplificada.
+        <p>A Germinne surgiu com o objetivo de incentivar cada vêz mais pessoas parte de um movimento que transforma os espaços urbanos em fontes de vida e saúde. Nossa plataforma promove informações e incentiva iniciativas que promovam a agricultura urbana: Leia os mais diversos artigos em nosso blog, converse com nosso chatbot inteligente que irá te orientar e ajudar a resolver os seus problemas, invista em nosso kit de cultivo indoor e plante em qualquer lugar de maneira simplificada.
         </p>
 
         <p>Temos como objetivo tornar a agricutura urbana mais acessível, inclusiva e produtiva para todos.</p>
