c31f803e57db39e01f2445f67df47fa9b184f14d
Vitor Moutim <moutimg@gmail.com>
2023-10-31 17:47:41 -0300

    feat: implementa pagina sobre nos com formulario para contato

--- src/components/FormsSobreNos/FormsSobreNos.jsx
+++ src/components/FormsSobreNos/FormsSobreNos.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
+import './formsSobreNos.css';
 
 function FormsSobreNos() {
   const [state, handleSubmit] = useForm("xaygkwra");
@@ -9,14 +10,14 @@ function FormsSobreNos() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className='formulario-duvidas'>
       <div>
         <label htmlFor="nome">
-          Nome
           <input
             id="nome"
             type="text"
             name="nome"
+            placeholder='Digite seu nome'
           />
         </label>
         <ValidationError
@@ -28,11 +29,11 @@ function FormsSobreNos() {
 
       <div>
         <label htmlFor="email">
-          Email
           <input
             id="email"
             type="email"
             name="email"
+            placeholder='Digite seu email'
           />
         </label>
         <ValidationError
@@ -44,11 +45,11 @@ function FormsSobreNos() {
 
       <div>
         <label htmlFor="assunto">
-          Assunto
           <input
             id="assunto"
             type="text"
             name="assunto"
+            placeholder='Qual assunto você quer tratar?'
           />
         </label>
         <ValidationError
@@ -60,10 +61,10 @@ function FormsSobreNos() {
 
       <div>
         <label htmlFor="mensagem">
-          Digite sua mensagem
           <textarea
             id="mensagem"
             name="mensagem"
+            placeholder='Digite sua mensagem...'
           />
         </label>
         <ValidationError
@@ -75,7 +76,7 @@ function FormsSobreNos() {
       <button type="submit" disabled={state.submitting}>
         Enviar mensagem
       </button>
-    </form>
+    </form >
   );
 }
 
