e07094ad89c725e7be77d0c563ba62a605067d67
Vitor Moutim <moutimg@gmail.com>
2023-10-30 21:04:22 -0300

    fix: typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './pages/Home/Home';
-import Header from './components/header/Header';
+import Header from './components/Header/Header';
 import './App.css';
 import Footer from './components/Footer/Footer';
 
