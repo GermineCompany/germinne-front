df6c7a09c373b84fef548659a1b59deba74d0f0b
Sidney Franca <sidneyrdfranca@gmail.com>
2023-11-18 18:49:48 -0300

    fix: FAQ corrigido

--- src/pages/Loja/Loja.jsx
+++ src/pages/Loja/Loja.jsx
@@ -50,26 +50,24 @@ function Loja() {
       </div>
 
 
-      <div className='box-card-pergunta'>
-
-
-
-
-
-        <CardPerguntas 
-          title='Quantas horas de sol por dia minha planta precisa?'
-          text='Para que suas plantinhas tenham o desenvolvimento ideal, você vai precisar de pelo menos 4 horas de sol direto ao dia, e não basta ser só aquela claridade, é necessário que realmente a luz do sol incidir diretamente sobre sua hortinha.'  
-        />
-        <hr />
-        <CardPerguntas  
-          title='Quanto tempo leva para germinar?'  
-          text='Vai depender do tipo da semente. Disponibilizamos em nosso blog informações completinhas sobre diversas plantas, confira aqui.'
-        />
-        <hr />
-        <CardPerguntas  
-          title='Preciso regar?' 
-          text='Nosso kit conta com um sistema de autoirrigação, que facilita o cuidado das plantas ao fornecer água de forma automática. Então se você tem pouco tempo disponível para regar plantas ou se não têm experiência prévia no cultivo não se preocupe, nosso kit não precisa de maiores trabalhos :)'  
-        />
+      <div className='faq'>
+        <h2 className='faq-title'>Perguntas frequentes</h2>
+        <div className='box-card-pergunta'>
+          <CardPerguntas 
+            title='Quantas horas de sol por dia minha planta precisa?'
+            text='Para que suas plantinhas tenham o desenvolvimento ideal, você vai precisar de pelo menos 4 horas de sol direto ao dia, e não basta ser só aquela claridade, é necessário que realmente a luz do sol incidir diretamente sobre sua hortinha.'  
+          />
+          <hr />
+          <CardPerguntas  
+            title='Quanto tempo leva para germinar?'  
+            text='Vai depender do tipo da semente. Disponibilizamos em nosso blog informações completinhas sobre diversas plantas, confira aqui.'
+          />
+          <hr />
+          <CardPerguntas  
+            title='Preciso regar?' 
+            text='Nosso kit conta com um sistema de autoirrigação, que facilita o cuidado das plantas ao fornecer água de forma automática. Então se você tem pouco tempo disponível para regar plantas ou se não têm experiência prévia no cultivo não se preocupe, nosso kit não precisa de maiores trabalhos :)'  
+          />
+        </div>
       </div>
     </div>
   );
