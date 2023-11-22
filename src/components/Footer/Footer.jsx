320ff3b19a5c49b78e09e82958fd4dbe81e42a3e
João Pedro Loureiro <jploureiro2004@gmail.com>
2023-11-22 19:33:44 -0300

    style: estrutura e css refeitos

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,58 +1,65 @@
-import React from 'react';
-import './footer.css';
-import logo from '../../images/logo-branco.svg';
-import {  RiLinkedinBoxFill } from 'react-icons/ri';
-import { SiLinktree } from 'react-icons/si';
-import { FaInstagram } from 'react-icons/fa';
-
-
-
-
+import React from "react";
+import "./footer.css";
+import logo from "../../images/logo-branco.svg";
+import { RiLinkedinBoxFill } from "react-icons/ri";
+import { SiLinktree } from "react-icons/si";
+import { FaInstagram } from "react-icons/fa";
 
 function Footer() {
   return (
     <footer>
-      <div className='footer-box'>
-        <div className='footer-logo'>
-          <img className='LogoGerminne' src={ logo } alt="Logo da Germinne branco" />
-          <h2>Germinne</h2>
-        </div>
-        <div className='redes'>
-          <a href="https://www.instagram.com/germinne_oficial/">
-            <FaInstagram className='footer-icons'/>
-          </a>
-          <a href="https://www.linkedin.com/in/vitormoutim/">
-            <RiLinkedinBoxFill className='footer-icons'/>
-          </a>
-          <a href="https://www.linkedin.com/in/vitormoutim/">
-            <SiLinktree className='footer-icons'/>
-          </a>
+      <div className="footer-box">
+        <div>
+          <div className="footer-logo">
+            <img src={logo} alt="Logo da Germinne branco" />
+            <h2>Germinne</h2>
+          </div>
 
-        </div>
-        <div className='Contatos'>
-          <p href=""> <a className='ancoras'href=""> BLOG GERMINNE </a></p>
-          <p href=""> <a className='ancoras'href=""> EMAIL </a></p>
-          <p href=""> <a className='ancoras'href=""> TELEFONE </a></p>
-          <p href=""> <a className='ancoras'href=""> SOBRE NÓS </a></p>
-        </div>
-        <div className='Site'>
-          <p href=""> <a className='ancoras'href=""> SEGURANÇA </a></p>
-          <p href=""> <a className='ancoras'href=""> TERMOS DE USO </a></p>
-          <p href=""> <a className='ancoras'href=""> POLITICAS DE ENVIO </a></p>
-          <p href=""> <a className='ancoras'href=""> PERGUNTAS FREQUENTES </a></p>
+          <div className="redes">
+            <a href="https://www.instagram.com/germinne_oficial/">
+              <FaInstagram className="footer-icons" />
+            </a>
+            <a href="https://www.linkedin.com/in/vitormoutim/">
+              <RiLinkedinBoxFill className="footer-icons" />
+            </a>
+            <a href="https://www.linkedin.com/in/vitormoutim/">
+              <SiLinktree className="footer-icons" />
+            </a>
+          </div>
         </div>
 
+        <div>
+          <ul>
+            <li>
+              <a href="">BLOG GERMINNE</a>
+            </li>
+            <li>
+              <a href="">EMAIL</a>
+            </li>
+            <li>
+              <a href="">TELEFONE</a>
+            </li>
+            <li>
+              <a href="">SOBRE NÓS</a>
+            </li>
+          </ul>
 
-
+          <ul>
+            <li>
+              <a href="">SEGURANÇA</a>
+            </li>
+            <li>
+              <a href="">TERMOS DE USO</a>
+            </li>
+            <li>
+              <a href="">POLITICAS DE ENVIO</a>
+            </li>
+            <li>
+              <a href="">PERGUNTAS FREQUENTES</a>
+            </li>
+          </ul>
+        </div>
       </div>
-
-
-
-
-
-       
-   
-        
     </footer>
   );
 }
