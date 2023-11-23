0c8e36dffb183b322d1a858430c7e02effcc9c07
João Pedro Loureiro <jploureiro2004@gmail.com>
2023-11-23 01:57:44 -0300

    feat: criando banner

---
+++ src/pages/TrabalheConosco/TrabalheConosco.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./trabalheConosco.css";
+
+function TrabalheConosco() {
+  return (
+    <div>
+      <div className="banner-trabalhe-conosco">
+        <div>
+          <h1>
+            Conquiste novos clientes divulgando seu trabalho na nossa plataforma
+          </h1>
+          <div className="botoes-trabalhe-conosco">
+            <button>Cadastre-se</button>
+            <button>Profissionais</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TrabalheConosco;
