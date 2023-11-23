ceed6ee15c56d470056ca13bb6da5d03f63b0d1a
João Pedro Loureiro <jploureiro2004@gmail.com>
2023-11-23 04:58:29 -0300

    feat: finalizado banner e feito custo beneficio

--- src/pages/TrabalheConosco/TrabalheConosco.jsx
+++ src/pages/TrabalheConosco/TrabalheConosco.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./trabalheConosco.css";
+import CustoBeneficio from "../../images/custo-beneficio-trabalhe-conosco.svg";
 
 function TrabalheConosco() {
   return (
-    <div>
+    <div className="box-trabalhe-conosco">
       <div className="banner-trabalhe-conosco">
         <div>
           <h1>
@@ -13,6 +14,21 @@ function TrabalheConosco() {
             <button>Cadastre-se</button>
             <button>Profissionais</button>
           </div>
+        </div>
+      </div>
+
+      <div className="custoBeneficio">
+        <img src={CustoBeneficio} alt="" />
+        <div>
+          <h2>Custo beneficio e acesso ao seu publico alvo</h2>
+          <p>
+            Invista apenas R$ xx,xx por mês e impulsione suas receitas com a
+            atração de novos clientes, nossa plataforma é acessada diariamente
+            por pessoas engajadas e envolvidas em práticas sustentáveis.
+          </p>
+          <a href="#">
+            <button>Quero me cadastrar</button>
+          </a>
         </div>
       </div>
     </div>
