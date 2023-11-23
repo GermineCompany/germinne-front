5ff081b1db3cecf1472b8e9dc0474818613bc992
Aluno <Victorgs199@gmail.com>
2023-11-23 04:56:32 -0300

    feat: Componente BeneficiosParaVoce

--- src/pages/TrabalheConosco/TrabalheConosco.jsx
+++ src/pages/TrabalheConosco/TrabalheConosco.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./trabalheConosco.css";
+import BeneficiosParaVoce from "../../components/BeneficiosParaVoce/BeneficiosParaVoce";
+import img1 from "../../images/img-beneficio1.svg"
+import img2 from "../../images/imagemBeneficio2.svg"
+import img3 from "../../images/imagemBeneficio3.svg"
 
 function TrabalheConosco() {
   return (
@@ -15,6 +19,27 @@ function TrabalheConosco() {
           </div>
         </div>
       </div>
+      <div className="ElementoComponente">
+      <BeneficiosParaVoce 
+      urlImg={img1}
+      altImg="beneficio1"
+      titulo="Aumente sua visibilidade"
+      texto="Nossa plataforma te conecta com uma audiência diária de centenas de pessoas."
+      />
+      <BeneficiosParaVoce 
+      urlImg={img2}
+      altImg="beneficio1"
+      titulo="Sem comissão"
+      texto="Não cobramos comissões dos serviços realizados, 100% do valor fica com você!."
+      />
+       <BeneficiosParaVoce 
+      urlImg={img3}
+      altImg="beneficio1"
+      titulo="Contato direto"
+      texto="Converse e negocie diretamente, sem intermediadores, sem taxas e sem burocracia."
+      />
+      </div>
+  
     </div>
   );
 }
