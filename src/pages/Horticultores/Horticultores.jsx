63eaa628083c638be2b0f3afa46e59db5ef70de0
Vitor Moutim <moutimg@gmail.com>
2023-11-23 05:49:20 -0300

    fix: corrige erro de margem dos elementos na tela de horticultores

--- src/pages/Horticultores/Horticultores.jsx
+++ src/pages/Horticultores/Horticultores.jsx
@@ -57,67 +57,69 @@ function Horticultores() {
         </div>
       </div>
 
-      <div className='box-como-funciona'>
-        <img className="como-funciona-mainImg" src={imgComoFunciona} alt="imgComoFunciona" />
+      <div className='main-container-limitador'>
+        <div className='box-como-funciona'>
+          <img className="como-funciona-mainImg" src={imgComoFunciona} alt="imgComoFunciona" />
 
-        <div className='box-texto-como-funciona'>
-          <h2>Como funciona?</h2>
-          <ComoFunciona
-            urlImg={Passo1}
-            altImg="icone1"
-            conteudo="Responda as questões sobre o tipo de trabalho, onde será feito e o tamanho aproximado do espaço."
-          />
+          <div className='box-texto-como-funciona'>
+            <h2>Como funciona?</h2>
+            <ComoFunciona
+              urlImg={Passo1}
+              altImg="icone1"
+              conteudo="Responda as questões sobre o tipo de trabalho, onde será feito e o tamanho aproximado do espaço."
+            />
 
-          <ComoFunciona
-            urlImg={Passo2}
-            altImg="icone2"
-            conteudo="Acesse serviços de horticultura e tenha hortas facilmente acessíveis!"
-          />
+            <ComoFunciona
+              urlImg={Passo2}
+              altImg="icone2"
+              conteudo="Acesse serviços de horticultura e tenha hortas facilmente acessíveis!"
+            />
 
-          <ComoFunciona
-            urlImg={Passo3}
-            altImg="icone3"
-            conteudo="Entre em contato direto com o nosso parceiro através do WhatsApp ou Chat integrado na plataforma e feche os detalhes do serviço."
-          />
+            <ComoFunciona
+              urlImg={Passo3}
+              altImg="icone3"
+              conteudo="Entre em contato direto com o nosso parceiro através do WhatsApp ou Chat integrado na plataforma e feche os detalhes do serviço."
+            />
 
-          <ComoFunciona
-            urlImg={Passo4}
-            altImg="icone4"
-            conteudo="Após aprovar o orçamento nosso parceiro vai realizar o serviço na data e local combinados. "
-          />
-        </div >
+            <ComoFunciona
+              urlImg={Passo4}
+              altImg="icone4"
+              conteudo="Após aprovar o orçamento nosso parceiro vai realizar o serviço na data e local combinados. "
+            />
+          </div >
 
-      </div>
+        </div>
 
-      <div className='box-servicos'>
+        <div className='box-servicos'>
 
-        <h2><span>Promovemos</span> diversos serviços:</h2>
+          <h2><span>Promovemos</span> diversos serviços:</h2>
 
-        <div className='box-card-servicos-horticultores'>
-          <ArticleBeneficios
-            urlImg={servico1}
-            altImg='Imgem de um homem plantando'
-            title='Plantio e replantio'
-            text='Se você sonha em transformar espaços inutilizados em deslumbrantes hortas orgânicas, nossa equipe está pronta para tornar isso realidade! O plantio e replantio ganham vida com o toque especial dos nossos especialistas, que não só cultivam plantas, mas criam ambientes vibrantes e cheios de vida.'
-          />
+          <div className='box-card-servicos-horticultores'>
+            <ArticleBeneficios
+              urlImg={ servico1 }
+              altImg='Imgem de um homem plantando'
+              title='Plantio e replantio'
+              text='Se você sonha em transformar espaços inutilizados em deslumbrantes hortas orgânicas, nossa equipe está pronta para tornar isso realidade! O plantio e replantio ganham vida com o toque especial dos nossos especialistas, que não só cultivam plantas, mas criam ambientes vibrantes e cheios de vida.'
+            />
 
-          <ArticleBeneficios
-            urlImg={servico2}
-            altImg='Imagem de várias comidas em uma mesa'
-            title='Controle de pragas e enfermidades'
-            text='Nossos profissionais não apenas conhecem o significado de "Proteção" mas sim a linguagem secreta das plantas. Com expertise em controle de pragas e enfermidades, eles protegem seu jardim como guardiões dedicados.'
-          />
+            <ArticleBeneficios
+              urlImg={ servico2 }
+              altImg='Imagem de várias comidas em uma mesa'
+              title='Controle de pragas e enfermidades'
+              text='Nossos profissionais não apenas conhecem o significado de "Proteção" mas sim a linguagem secreta das plantas. Com expertise em controle de pragas e enfermidades, eles protegem seu jardim como guardiões dedicados.'
+            />
 
-          <ArticleBeneficios
-            urlImg={servico3}
-            altImg='Imagem de várias comidas em uma mesa'
-            title='Manutenção e fertilização'
-            text='Imagine uma Horta que sempre pareceu saído de uma revista de jardinagem - agora, pare de imaginar e comece a viver essa realidade! Com a manutenção e fertilização especializada dos nossos profissionais, seu espaço verde será constantemente nutrido e mimado.'
-          />
+            <ArticleBeneficios
+              urlImg={ servico3 }
+              altImg='Imagem de várias comidas em uma mesa'
+              title='Manutenção e fertilização'
+              text='Imagine uma Horta que sempre pareceu saído de uma revista de jardinagem - agora, pare de imaginar e comece a viver essa realidade! Com a manutenção e fertilização especializada dos nossos profissionais, seu espaço verde será constantemente nutrido e mimado.'
+            />
+          </div>
+
         </div>
 
       </div>
-
       <div className='box-banner-cadastro-horticultor'>
         <div className='banner-cadastro-horticultor'>
           <div>
