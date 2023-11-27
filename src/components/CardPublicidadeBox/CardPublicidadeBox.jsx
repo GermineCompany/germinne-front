faa4a4204b3c9f30cbd39a11ff76f65827ba20d4
Sidney Franca <sidneyrdfranca@gmail.com>
2023-11-27 03:59:53 -0300

    style: grande refatoração na pagina de artigos e seus componentes

--- src/components/CardPublicidadeBox/CardPublicidadeBox.jsx
+++ src/components/CardPublicidadeBox/CardPublicidadeBox.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './cardPublicidadeBox.css';
+import PropTypes from 'prop-types';
 
-function CardPublicidadeBox() {
+function CardPublicidadeBox({ srcImg, altImg }) {
   return (
-    <div className='card-publicidade-box'>
-      <h5><span>Germinne</span> você também!</h5>
-      <button>Conheça</button>
+    <div className="card-publicidade-box">
+      <img src={srcImg} alt={altImg} />
     </div>
   );
 }
+
+CardPublicidadeBox.propTypes = {
+  srcImg: PropTypes.string.isRequired,
+  altImg: PropTypes.string.isRequired,
+};
 
 export default CardPublicidadeBox;
--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import busca from '../../images/busca.png';
-import publicidade from '../../images/publi.png';
+import publicidade from '../../images/card-publicidade-germinne.png';
 import CardBlog from '../../components/CardBlog/CardBlog';
 import CardPublicidade from '../../components/CardPublicidade/CardPublicidade';
 import './blog.css';
 import CarouselBlog from '../../components/CarouselBlog/CarouselBlog';
 import artigos from '../../data/artigos';
-
 
 function Blog() {
   const [buscar, setBuscar] = useState('');
@@ -14,61 +13,62 @@ function Blog() {
   const handleChange = (event) => setBuscar(event.target.value);
 
   return (
-    <div className='pagina-blog'>
-      <article className='carousel-blog'>
-        <CarouselBlog />
-      </article>
+    <div className="main-container-limitador">
+      <div className="pagina-blog">
+        <article className="carousel-blog">
+          <CarouselBlog />
+        </article>
 
-      <hr />
+        <hr />
 
-      <div className='filtros'>
-        <p>Todos</p>
-        <p>Agricultura urbana</p>
-        <p>Cultivo</p>
-        <p>Curiosidades</p>
-      </div>
+        <div className="filtros">
+          <p>Todos</p>
+          <p>Agricultura urbana</p>
+          <p>Cultivo</p>
+          <p>Curiosidades</p>
+        </div>
 
-      <div className='busca'>
-        <div>
-          <div className='icone-lupa'>
-            <img src={busca} alt="Icone de uma lupa" />
+        <div className="busca">
+          <div>
+            <div className="icone-lupa">
+              <img src={busca} alt="Icone de uma lupa" />
+            </div>
+            <input
+              onChange={handleChange}
+              type="text"
+              name="busca"
+              id="busca"
+              placeholder="Pesquise no blog"
+              value={buscar}
+            />
           </div>
-          <input
-            onChange={handleChange}
-            type="text"
-            name="busca"
-            id="busca"
-            placeholder='Pesquise no blog'
-            value={buscar}
-          />
+        </div>
+
+        <div className="box-artigos">
+          {artigos.map(
+            ({ artigoId, titulo, descricao, imagemCard, categoria }) => {
+              return (
+                <CardBlog
+                  key={artigoId}
+                  srcImg={imagemCard}
+                  altImg="Foto do telhado verde"
+                  title={titulo}
+                  resume={descricao}
+                  category={categoria}
+                  idArticle={artigoId}
+                />
+              );
+            }
+          )}
+        </div>
+
+        <hr />
+
+        <div className="publicidade">
+          <CardPublicidade srcImg={publicidade} />
+          <CardPublicidade srcImg={publicidade} />
         </div>
       </div>
-
-      <div className='box-artigos'>
-        {
-          artigos.map(({ artigoId, titulo, descricao, imagemCard, categoria }) => {
-            return (
-              <CardBlog
-                key={artigoId}
-                srcImg={ imagemCard }
-                altImg="Foto do telhado verde"
-                title={ titulo }
-                resume={ descricao }
-                category={ categoria }
-                idArticle={ artigoId }
-              />
-            );
-          })
-        }
-      </div>
-
-      <hr />
-
-      <div className='publicidade'>
-        <CardPublicidade srcImg={ publicidade }/>
-        <CardPublicidade srcImg={ publicidade }/>
-      </div>
-
     </div>
   );
 }
