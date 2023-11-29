7cca59d7ec8369dc77300cd89671f369c6390c5d
Vitor Moutim <moutimg@gmail.com>
2023-11-29 15:17:01 -0300

    fix: corrige o erro dos elementos nao estarem fechados

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -43,101 +43,103 @@ function Home() {
               routeName="loja"
             />
           </div>
-
-      <div className='main-container-limitador'>
-        <article className='box-card-home'>
-          <div className='box-card-servicos'>
-            <CardHomeServicos 
-              title='Explore informações sobre a agricultura'
-              subtitle={['Acesse o ', <span key={ 1 }>Germinne Blog</span>]}
-              className='first-service-card'
-              routeName='blog'
-            />
-            <CardHomeServicos
-              title='Contrate profissionais de confinaça'
-              subtitle={['Profissionais de ', <span key={ 2 }>Qualidade</span>]}
-              className='second-service-card'
-              routeName='horticultores'
-            />
-            <CardHomeServicos
-              title='Plante sem dificuldades'
-              subtitle={['Conheça a ', <span key={ 3 }>Germinne Box</span>]}
-              className='third-service-card'
-              routeName='loja'
-            />
-          </div>
-
-          <div className='box-card-germinne'>
-            <CardHomeGerminne
-              title={['Temos a missão de levar o ', <span key={ 1 }>verde</span>, ' para as áreas urbanas']}
-              className='first-germinne-card'
-            />
-
-            <CardHomeGerminne
-              title={['Promovemos a ',<span key={ 2 }>sustentabilidade</span>, ' contribuindo para uma cidade mais ' ,<span key={ 2 }>saudável</span>,]}
-              className='second-germinne-card'
-              subtitle={['Conheça a ', <span key={ 4 }>Germinne</span>]}
-              id='second-germinne-large'
-            />
-
-            <CardHomeGerminne
-              title={['Temos a missão de levar o ', <span key={ 3 }>verde</span>, ' para as áreas urbanas']}
-              className='third-germinne-card'
-            />
-          </div>
         </article>
 
-        <section className='box-beneficios-agricultura'>
-          <h2>Benefícios da <span>Agricultura Urbana</span></h2>
+        <div className='main-container-limitador'>
+          <article className='box-card-home'>
+            <div className='box-card-servicos'>
+              <CardHomeServicos 
+                title='Explore informações sobre a agricultura'
+                subtitle={['Acesse o ', <span key={ 1 }>Germinne Blog</span>]}
+                className='first-service-card'
+                routeName='blog'
+              />
+              <CardHomeServicos
+                title='Contrate profissionais de confinaça'
+                subtitle={['Profissionais de ', <span key={ 2 }>Qualidade</span>]}
+                className='second-service-card'
+                routeName='horticultores'
+              />
+              <CardHomeServicos
+                title='Plante sem dificuldades'
+                subtitle={['Conheça a ', <span key={ 3 }>Germinne Box</span>]}
+                className='third-service-card'
+                routeName='loja'
+              />
+            </div>
 
-          <ArticleBeneficios
-            urlImg={ articleImage1 }
-            altImg='Imagem de várias comidas em uma mesa'
-            title='Alimentos mais frescos e nutritivos'
-            text='Os alimentos cultivados localmente são colhidos no auge da maturidade, garantindo maior frescor e preservação dos nutrientes, proporcionando opções mais saudáveis para os consumidores.'
-          />
+            <div className='box-card-germinne'>
+              <CardHomeGerminne
+                title={['Temos a missão de levar o ', <span key={ 1 }>verde</span>, ' para as áreas urbanas']}
+                className='first-germinne-card'
+              />
 
-          <ArticleBeneficios
-            urlImg={ articleImage2 }
-            altImg='Imagem de pessoas trabalhando em uma horta'
-            title='Sustentabilidade ambiental'
-            text='Ao aproveitar espaços urbanos para o cultivo, a agricultura urbana contribui para a redução da pegada de carbono, promove a biodiversidade e cria ambientes mais sustentáveis nas cidades.'
-          />
+              <CardHomeGerminne
+                title={['Promovemos a ',<span key={ 2 }>sustentabilidade</span>, ' contribuindo para uma cidade mais ' ,<span key={ 2 }>saudável</span>,]}
+                className='second-germinne-card'
+                subtitle={['Conheça a ', <span key={ 4 }>Germinne</span>]}
+                id='second-germinne-large'
+              />
 
-          <ArticleBeneficios
-            urlImg={ articleImage3 }
-            altImg='Imagem de uma pessoa com luvas trabalhando em uma horta'
-            title='Cultivar plantas: um impulso positivo para a saúde mental'
-            text='Plantar faz bem para a cabeça! Reduz estresse e ansiedade, trazendo alegria com a natureza. Horticultura dá sensação boa, deixando a gente mais feliz. Cuidar das plantas e ter rotinas de plantio é tipo uma terapia, criando um ambiente que faz a gente se sentir bem.'
-          />
-        </section>
+              <CardHomeGerminne
+                title={['Temos a missão de levar o ', <span key={ 3 }>verde</span>, ' para as áreas urbanas']}
+                className='third-germinne-card'
+              />
+            </div>
+          </article>
 
-        <section className='box-avaliacoes'>
-          <h3><span>Avaliações</span> dos nossos <span>clientes</span></h3>
+          <section className='box-beneficios-agricultura'>
+            <h2>Benefícios da <span>Agricultura Urbana</span></h2>
 
-          <div>
-            <CardAvaliacao 
-              srcImg='https://media.licdn.com/dms/image/C4D03AQFmj98FQmRvrA/profile-displayphoto-shrink_800_800/0/1662045322237?e=1702512000&v=beta&t=HlsPx_deG_QLg0MTJJNiK8aaT4mJ7AVKrcCCabPb50I'
-              altImg='Foto do Gabriel Augusto'
-              description='"Me tornei pai de planta com a Germine Box"'
-              name='Gabriel Augusto'
+            <ArticleBeneficios
+              urlImg={ articleImage1 }
+              altImg='Imagem de várias comidas em uma mesa'
+              title='Alimentos mais frescos e nutritivos'
+              text='Os alimentos cultivados localmente são colhidos no auge da maturidade, garantindo maior frescor e preservação dos nutrientes, proporcionando opções mais saudáveis para os consumidores.'
             />
 
-            <CardAvaliacao 
-              srcImg='https://media.licdn.com/dms/image/C4D03AQFmj98FQmRvrA/profile-displayphoto-shrink_800_800/0/1662045322237?e=1702512000&v=beta&t=HlsPx_deG_QLg0MTJJNiK8aaT4mJ7AVKrcCCabPb50I'
-              altImg='Foto do Gabriel Augusto'
-              description='“Comprei a GerminneBox e foi otimo, pouco trabalho, quase nenhum.”'
-              name='Gabriel Silva'
+            <ArticleBeneficios
+              urlImg={ articleImage2 }
+              altImg='Imagem de pessoas trabalhando em uma horta'
+              title='Sustentabilidade ambiental'
+              text='Ao aproveitar espaços urbanos para o cultivo, a agricultura urbana contribui para a redução da pegada de carbono, promove a biodiversidade e cria ambientes mais sustentáveis nas cidades.'
             />
 
-            <CardAvaliacao 
-              srcImg='https://media.licdn.com/dms/image/C4D03AQFmj98FQmRvrA/profile-displayphoto-shrink_800_800/0/1662045322237?e=1702512000&v=beta&t=HlsPx_deG_QLg0MTJJNiK8aaT4mJ7AVKrcCCabPb50I'
-              altImg='Foto do Gabriel Augusto'
-              description='"Encontrei um profssional na vitrine da Germinne e "'
-              name='Gabriel Lindo Augusto'
+            <ArticleBeneficios
+              urlImg={ articleImage3 }
+              altImg='Imagem de uma pessoa com luvas trabalhando em uma horta'
+              title='Cultivar plantas: um impulso positivo para a saúde mental'
+              text='Plantar faz bem para a cabeça! Reduz estresse e ansiedade, trazendo alegria com a natureza. Horticultura dá sensação boa, deixando a gente mais feliz. Cuidar das plantas e ter rotinas de plantio é tipo uma terapia, criando um ambiente que faz a gente se sentir bem.'
             />
-          </div>
-        </section>
+          </section>
+
+          <section className='box-avaliacoes'>
+            <h3><span>Avaliações</span> dos nossos <span>clientes</span></h3>
+
+            <div>
+              <CardAvaliacao 
+                srcImg='https://media.licdn.com/dms/image/C4D03AQFmj98FQmRvrA/profile-displayphoto-shrink_800_800/0/1662045322237?e=1702512000&v=beta&t=HlsPx_deG_QLg0MTJJNiK8aaT4mJ7AVKrcCCabPb50I'
+                altImg='Foto do Gabriel Augusto'
+                description='"Me tornei pai de planta com a Germine Box"'
+                name='Gabriel Augusto'
+              />
+
+              <CardAvaliacao 
+                srcImg='https://media.licdn.com/dms/image/C4D03AQFmj98FQmRvrA/profile-displayphoto-shrink_800_800/0/1662045322237?e=1702512000&v=beta&t=HlsPx_deG_QLg0MTJJNiK8aaT4mJ7AVKrcCCabPb50I'
+                altImg='Foto do Gabriel Augusto'
+                description='“Comprei a GerminneBox e foi otimo, pouco trabalho, quase nenhum.”'
+                name='Gabriel Silva'
+              />
+
+              <CardAvaliacao 
+                srcImg='https://media.licdn.com/dms/image/C4D03AQFmj98FQmRvrA/profile-displayphoto-shrink_800_800/0/1662045322237?e=1702512000&v=beta&t=HlsPx_deG_QLg0MTJJNiK8aaT4mJ7AVKrcCCabPb50I'
+                altImg='Foto do Gabriel Augusto'
+                description='"Encontrei um profssional na vitrine da Germinne e "'
+                name='Gabriel Lindo Augusto'
+              />
+            </div>
+          </section>
+        </div>
       </div>
     </div>
   );
