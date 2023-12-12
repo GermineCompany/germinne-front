e9f57facf196758d14279d4a9bb99972152bdd4b
Joao Pedro <your_email@users.noreply.github.com>
2023-12-12 09:16:36 -0300

    style: arrumando header mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import logo from '../../images/logo-verde.svg';
-import sacola from '../../images/sacola.png';
-import './header.css';
-import HeaderMobile from '../HeaderMobile/HeaderMobile';
-import { useState } from 'react';
-import { useLocation } from 'react-router-dom';
-
+import React from "react";
+import logo from "../../images/logo-verde.svg";
+import sacola from "../../images/sacola.png";
+import "./header.css";
+import HeaderMobile from "../HeaderMobile/HeaderMobile";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
 
 function Header() {
   const [showLogin, setShowLogin] = useState(false);
@@ -13,16 +12,15 @@ function Header() {
 
   const handleShowLogin = () => {
     setShowLogin(showLogin ? false : true);
-  }; 
+  };
 
   return (
     <>
       <HeaderMobile id="component-mobile" />
 
-      <header className='header-desktop'>
-
+      <header className="header-desktop">
         <div>
-          <div className='box-logo-nome'>
+          <div className="box-logo-nome">
             <img src={logo} alt="Logo verde" />
             <a href="/">
               <h1>GERMINNE</h1>
@@ -31,55 +29,90 @@ function Header() {
 
           <nav>
             <ul>
-              
-              <li className= "nav-item" >
-                <a href="/">Inicio<div className={ ` underline  ${pathname == '/' ? 'hold-underline' : '' }`} /></a>
+              <li className="nav-item">
+                <a href="/">
+                  Inicio
+                  <div
+                    className={` underline  ${
+                      pathname == "/" ? "hold-underline" : ""
+                    }`}
+                  />
+                </a>
               </li>
 
-              <li className= "nav-item" >
-                <a href="/blog">Blog<div className={ ` underline  ${pathname == '/blog' ? 'hold-underline' : '' }`} /></a>
+              <li className="nav-item">
+                <a href="/blog">
+                  Blog
+                  <div
+                    className={` underline  ${
+                      pathname == "/blog" ? "hold-underline" : ""
+                    }`}
+                  />
+                </a>
               </li>
 
-              <li className= "nav-item" >
-                <a href="/loja">GerminneBox<div className={ ` underline  ${pathname == '/loja' ? 'hold-underline' : '' }`}/></a>
+              <li className="nav-item">
+                <a href="/loja">
+                  GerminneBox
+                  <div
+                    className={` underline  ${
+                      pathname == "/loja" ? "hold-underline" : ""
+                    }`}
+                  />
+                </a>
               </li>
 
-              <li className={' nav-item profissionais '}>
+              <li className={" nav-item profissionais "}>
                 <a href="">Profissionais</a>
-                <div className='dropdown-horticultores'>
-                  <a href="/horticultores">Contrate</a>
-                  <a href="/trabalhe-conosco">Faça parte</a>
+                <div className="dropdown-horticultores">
+                  <a href="/contrate">Contrate</a>
+                  <a href="/faca-parte">Faça parte</a>
                 </div>
               </li>
 
-              <li className= "nav-item" >
-                <a href="/sobre-nos">Sobre nós<div className={ ` underline  ${pathname == '/sobre-nos' ? 'hold-underline' : '' }`}/></a>
+              <li className="nav-item">
+                <a href="/sobre-nos">
+                  Sobre nós
+                  <div
+                    className={` underline  ${
+                      pathname == "/sobre-nos" ? "hold-underline" : ""
+                    }`}
+                  />
+                </a>
               </li>
-             
             </ul>
           </nav>
-        
-          <div className='box-login'>
-            <div>
-              <button className={ showLogin ? 'button-opened' : ''} onClick={ handleShowLogin }>Entrar</button>
 
-              <div className={`links-login ${showLogin ? 'open-login' : 'close-login'}`}>
+          <div className="box-login">
+            <div>
+              <button
+                className={showLogin ? "button-opened" : ""}
+                onClick={handleShowLogin}>
+                Entrar
+              </button>
+
+              <div
+                className={`links-login ${
+                  showLogin ? "open-login" : "close-login"
+                }`}>
                 <ul>
-                  <li><a href="/login-cliente">Cliente</a></li>
-                  <li><a href="/login">Profissional</a></li>
+                  <li>
+                    <a href="/login-cliente">Cliente</a>
+                  </li>
+                  <li>
+                    <a href="/login">Profissional</a>
+                  </li>
                 </ul>
               </div>
-
             </div>
-            
-            <div className='box-icone-sacola'>
+
+            <div className="box-icone-sacola">
               <a href="/carrinho">
                 <img src={sacola} alt="Icone de sacola" />
               </a>
             </div>
           </div>
         </div>
-
       </header>
     </>
   );
--- src/components/HeaderMobile/HeaderMobile.jsx
+++ src/components/HeaderMobile/HeaderMobile.jsx
@@ -1,27 +1,34 @@
-import React, { useState } from 'react';
-import logo from '../../images/logo-verde.svg';
-import sacola from '../../images/sacola.png';
-import fecharMenu from '../../images/fechar-menu.png';
-import menu from '../../images/menu.png';
-import './headerMobile.css';
-import 'animate.css';
-
+import React, { useState } from "react";
+import logo from "../../images/logo-verde.svg";
+import sacola from "../../images/sacola.png";
+import fecharMenu from "../../images/fechar-menu.png";
+import menu from "../../images/menu.png";
+import "./headerMobile.css";
+import "animate.css";
 
 function HeaderMobile() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   const handleMenu = () => setMenuOpen(menuOpen ? false : true);
 
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleShowDropdown = () => {
+    setShowDropdown(showDropdown ? false : true);
+    console.log("clicou");
+  };
+
   return (
     <>
-
-
-      <header className={!menuOpen ? '' : 'box-header-mobile'}>
-
-        <div className={!menuOpen ? 'esconder-menu-mobile animate__slideOutRight' : 'menu-mobile animate__animated animate__bounceInDown'}>
-
+      <header className={!menuOpen ? "" : "box-header-mobile"}>
+        <div
+          className={
+            !menuOpen
+              ? "esconder-menu-mobile animate__slideOutRight"
+              : "menu-mobile animate__animated animate__bounceInDown"
+          }>
           <div>
-            <div className='header-box'>
+            <div className="header-box">
               <img src={logo} alt="Logo verde" />
               <a href="/">
                 <h1>Germinne</h1>
@@ -30,7 +37,7 @@ function HeaderMobile() {
 
             <div>
               <img
-                className='icone-fechar'
+                className="icone-fechar"
                 onClick={handleMenu}
                 src={fecharMenu}
                 alt="Icone de um X"
@@ -40,29 +47,47 @@ function HeaderMobile() {
 
           <nav>
             <ul>
-              <li>Home</li>
-              <li>Blog</li>
-              <li>GerminneBox</li>
-              <li>Profissionais</li>
+              <li>
+                <a href="/home">Home</a>
+              </li>
+              <li>
+                <a href="/blog">Blog</a>
+              </li>
+              <li>
+                <a href="/loja">GerminneBox</a>
+              </li>
+              <li>
+                <a onClick={handleShowDropdown}>Profissionais</a>
+                <ul
+                  className={`${
+                    showDropdown ? "showDropdown-Mobile" : "hideDropdown-Mobile"
+                  }`}>
+                  <li>
+                    <a href="/contrate">Contrate</a>
+                  </li>
+                  <li>
+                    <a href="/faca-parte">Faça Parte</a>
+                  </li>
+                </ul>
+              </li>
               <li>Sobre nós</li>
             </ul>
           </nav>
-
         </div>
 
-        <div className={menuOpen ? 'esconder-menu-mobile' : 'header-mobile'}>
+        <div className={menuOpen ? "esconder-menu-mobile" : "header-mobile"}>
           <div>
             <img onClick={handleMenu} src={menu} alt="iconeMenu-Mobile" />
           </div>
 
-          <div className='header-box'>
+          <div className="header-box">
             <a href="/home">
               <img src={logo} alt="Logo verde" />
               <h1>Germinne</h1>
             </a>
           </div>
 
-          <div className='box-sacola'>
+          <div className="box-sacola">
             <div>
               <a href="/carrinho">
                 <img src={sacola} alt="Sacola verde" />
