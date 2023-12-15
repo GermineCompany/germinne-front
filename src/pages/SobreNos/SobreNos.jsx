b3e18d5552b29fb25191967f7b03afd73790081c
Sidney Franca <sidneyrdfranca@gmail.com>
2023-12-15 04:22:29 -0300

    PR aberto

--- src/pages/SobreNos/SobreNos.jsx
+++ src/pages/SobreNos/SobreNos.jsx
@@ -9,7 +9,6 @@ import FormsSobreNos from '../../components/FormsSobreNos/FormsSobreNos';
 
 function SobreNos() {
   return (
-    <article>
       <div className="box-text-sobre-nos">
         <h3>Sobre nós</h3>
         <p>
@@ -28,55 +27,6 @@ function SobreNos() {
           inclusiva e produtiva para todos.
         </p>
       </div>
-
-      <div className="box-sobre-nos">
-        <CardSobreNos
-          srcImg={missao}
-          altImg="Icone do missão"
-          title="Missão"
-          text="A nossa missão é promover e democratizar a agricultura urbana, fornecendo informações e práticas corretas."
-        />
-        <CardSobreNos
-          srcImg={visao}
-          altImg="Icone da visão"
-          title="Visão"
-          text="A nossa missão é promover e democratizar a agricultura urbana, fornecendo informações e práticas corretas."
-        />
-        <CardSobreNos
-          srcImg={valor}
-          altImg="Icone do valor"
-          title="Valor"
-          text="Ética, confiança, sustentabilidade, comprometimento, transparência, excelência, informações de qualidade e eficiência."
-        />
-      </div>
-
-      <div className="box-fale-conosco">
-        <h3>Tire suas dúvidas e fale conosco</h3>
-        <p>
-          Tem dúvidas quanto ao nosso Kit de Cultivo? Tem algum problema com seu
-          pedido? ou tem algum problema com suas plantinhas? Vamos te ajudar:
-        </p>
-
-        <div className="box-contatos">
-          <div>
-            <MdOutlineEmail />
-            <p>Nosso email</p>
-            <a href="mailto:germinne@contato.com">germinne@contato.com</a>
-          </div>
-
-          <div>
-            <MdOutlineWhatsapp />
-            <p>Nosso WhatsApp</p>
-            <a href="">Tire sua dúvida agora</a>
-          </div>
-        </div>
-
-        <div>
-          <h2>Ou preencha o formulário</h2>
-          <FormsSobreNos />
-        </div>
-      </div>
-    </article>
   );
 }
 
